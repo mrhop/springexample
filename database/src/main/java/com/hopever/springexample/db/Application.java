75aa87a23185d0efefe193762465be0ed21d1f2b
huodh <233402923@qq.com>
2016-01-29 22:11:03 +0800

    remove

---
+++ database/src/main/java/com/hopever/springexample/db/Application.java
@@ -0,0 +1,14 @@
+package com.hopever.springexample.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Donghui Huo on 2015/12/24.
+ */
+@SpringBootApplication
+public class Application {
+     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
