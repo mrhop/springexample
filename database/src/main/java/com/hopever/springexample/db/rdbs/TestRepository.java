233bd4923cb5f784b72f2eb3bec2a3e255212b85
mrhop <233402923@qq.com>
2016-02-25 12:33:00 +0800

    just update

---
+++ database/src/main/java/com/hopever/springexample/db/rdbs/TestRepository.java
@@ -0,0 +1,12 @@
+package com.hopever.springexample.db.rdbs;
+
+import com.hopever.springexample.db.jooq.tables.pojos.Test;
+import org.springframework.data.repository.CrudRepository;
+
+import java.io.Serializable;
+
+/**
+ * Created by Donghui Huo on 2016/2/25.
+ */
+public interface TestRepository extends CrudRepository<Test, Serializable> {
+}
