8335459c00c92a6bc04e62448c93008b0d15cf34
mrhop <233402923@qq.com>
2016-03-04 09:38:05 +0800

    Spring Integration

---
+++ integration/src/main/java/com/hopever/springexample/integration/http/RequestGateway.java
@@ -0,0 +1,8 @@
+package com.hopever.springexample.integration.http;
+
+/**
+ * Created by Donghui Huo on 2016/3/4.
+ */
+public interface RequestGateway {
+    String echo(String request);
+}
--- integration/src/main/java/com/hopever/springexample/integration/http/httpCommand.java
+++ integration/src/main/java/com/hopever/springexample/integration/http/httpCommand.java
@@ -11,10 +11,13 @@ import org.springframework.core.annotation.Order;
  */
 @Configuration
 @Order(9)
-@ImportResource("/META-INF/spring/integration/http/servlet-config.xml")
+@ImportResource({"/META-INF/spring/integration/http/servlet-config.xml","/META-INF/spring/integration/http/http-outbound-config.xml"})
 public class httpCommand extends UtilCommand implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         //still need a servlet for dispatcher
+        RequestGateway requestGateway = applicationContext.getBean("requestGateway", RequestGateway.class);
+        String reply = requestGateway.echo("Hello");
+        logger.info("\n\n++++++++++++ Replied with: " + reply + " ++++++++++++\n");
     }
 }
